0f4ed44d716f01cdf3759ee37ee766787e491578
Ítalo <italogholanda@gmail.com>
2021-08-28 14:23:40 -0300

    created startCountdown() function

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -1,18 +1,38 @@
 import cx from "classnames";
+import React from "react";
 import styles from "../styles/components/Countdown.module.css";
 
 export function Countdown() {
+  const [time, setTime] = React.useState(25 * 60);
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
+  const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
+  const [isActive, setIsActive] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isActive && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [isActive, time]);
+
+  function startCountdown() {
+    setIsActive(true);
+  }
+
   return (
     <>
       <div className={styles.countdown}>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </div>
 
@@ -22,17 +42,16 @@ export function Countdown() {
 
       <>
         <button
-        type="button"
-        className={cx(styles.startCycleButton,
-        styles.startCycleButtonActive)}>
+          type="button"
+          className={cx(styles.startCycleButton, styles.startCycleButtonActive)}
+        >
           Abandonar ciclo
         </button>
 
-        <button type="button" className={styles.startCycleButton}>
+        <button type="button" className={styles.startCycleButton} onClick={startCountdown}>
           Iniciar um ciclo
         </button>
       </>
-
     </>
   );
 }
